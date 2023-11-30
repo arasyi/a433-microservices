3b85b3e64132915a7694dee2a870f82ee33c2802
Mohammad Arif Rasyidi <arasyi@gmail.com>
2023-11-30 15:09:11 +0700

    Add script untuk build & push image karsajobs-ui

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Membuat docker image dari Dockerfile
+docker build -t ghcr.io/arasyi/karsajobs-ui:latest .
+
+# Login ke Github Packages menggunakan Personal Access Token (environment variable: GHCR_PAT)
+echo $GHCR_PAT | docker login ghcr.io -u arasyi --password-stdin
+
+# Mengunggah image ke Github Packages
+docker push ghcr.io/arasyi/karsajobs-ui:latest
